0fdc7352f78b8d8024c64d10b9453163b7fe15f6
Cicin2837512 <CicinGithub@skiff.com>
2024-05-19 16:20:13 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void enable_raw_mode(void);
+
+void enable_raw_mode(void)
+{
+
+}
+
+int main(void)
+{
+
+    enable_raw_mode();
+
+    return 0;
+}
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# run the code
+./build/text-editor
