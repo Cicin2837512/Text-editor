a7f0d836d619a96c04cca1e3e4dd2fe4f94a9f50
Cicin2837512 <CicinGithub@skiff.com>
2024-05-19 18:02:28 +0200

    actually first commit of useful code

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-*note: currently just in development*
+**note: currently just in development**
 
 # Purpose
 This is just a simple text editor I made to learn something about c.
+Heavily inspired by [kilo](https://github.com/antirez/kilo) and [this guide](https://viewsourcecode.org/snaptoken/kilo/)
--- src/main.c
+++ src/main.c
@@ -1,16 +1,66 @@
-#include <unistd.h>
+/*
+ * main.c
+ * it's just 1 file, no more info needed (check README.md and LICENSE)
+ */
 
-void enable_raw_mode(void);
+/* headers */
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* structs */
+typedef struct {
+    struct termios default_termios;
+} EditorConfig;
+
+/* function declarations */
+static void die(const char *s);
+static void disable_raw_mode(void); /* needs E.default_termis to be set */
+static void enable_raw_mode(void);
+
+/* global variables */
+EditorConfig E;
+
+/* main */
+int main(void)
+{
+    enable_raw_mode();
+    while (1) {
+        char c;
+        read(STDIN_FILENO, &c, 1);
+        if (c == 'q')
+            break;
+    }
+    return 0;
+}
+
+/* function definitions */
+static void die(const char *s)
+{
+    fprintf(stderr, "Error: %s", s);
+    exit(1);
+}
+
+void disable_raw_mode(void)
+{
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.default_termios) < 0)
+        die("tcsetattr");
+}
 
 void enable_raw_mode(void)
 {
+    if (tcgetattr(STDIN_FILENO, &E.default_termios) < 0)
+        die("tcgetattr");
+    atexit(disable_raw_mode);
 
-}
-
-int main(void)
-{
-
-    enable_raw_mode();
-
-    return 0;
+    struct termios raw_termios = E.default_termios;
+    raw_termios.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw_termios.c_iflag &= ~(IXON);
+    raw_termios.c_oflag &= ~(OPOST);
+    raw_termios.c_cflag |= (CS8);
+    raw_termios.c_cc[VMIN] = 0;
+    raw_termios.c_cc[VTIME] = 1;
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_termios) < 0)
+        die("tcsetattr");
 }
