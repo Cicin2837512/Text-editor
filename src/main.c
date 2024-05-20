3d85b5818a7a54a6a8f6bf674ba3901c027bec32
Cicin2837512 <CicinGithub@skiff.com>
2024-05-20 16:16:11 +0200

    added additional key movements

--- src/main.c
+++ src/main.c
@@ -23,6 +23,9 @@ enum EditorKeys {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN
 };
@@ -192,8 +195,13 @@ int read_key(void)
                 if (read(STDIN_FILENO, &seq[2], 1) < 0) return '\x1b';
                 if (seq[2] == '~') {
                     switch (seq[1]) {
+                    case '1': return HOME_KEY;
+                    case '3': return DEL_KEY;
+                    case '4': return END_KEY;
                     case '5': return PAGE_UP;
                     case '6': return PAGE_DOWN;
+                    case '7': return HOME_KEY;
+                    case '8': return END_KEY;
                     }
                 }
 
@@ -204,7 +212,14 @@ int read_key(void)
                 case 'B': return ARROW_DOWN;
                 case 'C': return ARROW_RIGHT;
                 case 'D': return ARROW_LEFT;
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
                 }
+            }
+        } else if (seq[0] == 'O') {
+            switch (seq[1]) {
+            case 'H': return HOME_KEY;
+            case 'F': return END_KEY;
             }
         }
 
@@ -244,6 +259,14 @@ void process_keypress(void)
     case CTRL_KEY('q'):
         write(STDOUT_FILENO, "\x1b[2J\x1b[H", 7);
         exit(0);
+        break;
+
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+
+    case END_KEY:
+        E.cx = E.screencols - 1;
         break;
 
     case PAGE_UP:
