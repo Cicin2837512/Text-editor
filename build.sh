67bb0f9ef5c9466ef1be98c2aa489856b343d76f
Cicin2837512 <CicinGithub@skiff.com>
2024-05-20 15:26:36 +0200

    added splash screen and movement with hjkl, added build.sh to build the file and modified the version in CMakeLists.txt

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /home/cicin/01Public/Text-editor/build
+cmake ..
+cmake --build .
--- src/main.c
+++ src/main.c
@@ -8,19 +8,38 @@
 #include <termios.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <string.h>
 
 /* macros */
 #define CTRL_KEY(x) ((x) & 0x1f)
+#define VERSION "v0.0.1"
 
 /* structs */
 typedef struct {
     struct termios default_termios;
+    int screenrows, screencols;
+    int cy, cx;
 } EditorConfig;
+
+typedef struct {
+    char *b;
+    size_t len;
+} String;
 
 /* function declarations */
 static void die(const char *s);
 static void disable_raw_mode(void); /* needs E.default_termis to be set */
 static void enable_raw_mode(void);
+static void get_window_size(int *rows, int *cols);
+static void s_append(String *sb, const char *s, size_t len);
+static void draw_rows(String *sb, int amount);
+static void refresh_screen(void);
+static char read_key(void);
+static void move_cursor(char key);
+static void process_keypress(void);
+static void init(void);
 
 /* global variables */
 EditorConfig E;
@@ -29,19 +48,18 @@ EditorConfig E;
 int main(void)
 {
     enable_raw_mode();
+    init();
     while (1) {
-        char c;
-        read(STDIN_FILENO, &c, 1);
-        if (c == CTRL_KEY('q'))
-            break;
+        refresh_screen();
+        process_keypress();
     }
     return 0;
 }
 
 /* function definitions */
-static void die(const char *s)
+void die(const char *s)
 {
-    fprintf(stderr, "Error: %s", s);
+    fprintf(stderr, "\x1b[2J\x1b[HError: %s", s);
     exit(1);
 }
 
@@ -66,4 +84,137 @@ void enable_raw_mode(void)
     raw_termios.c_cc[VTIME] = 1;
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_termios) < 0)
         die("tcsetattr");
+}
+
+void get_window_size(int *rows, int *cols)
+{
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) < 0 || ws.ws_col == 0) {
+        die("ioctl");
+    } else {
+        *rows = ws.ws_row;
+        *cols = ws.ws_col;
+    }
+}
+
+void s_append(String *sb, const char *s, size_t len)
+{
+    size_t new_len = sb->len + len;
+    char *b = (char *) malloc(new_len);
+    if (b == NULL)
+        die("malloc");
+    memcpy(b, sb->b, sb->len);
+    memcpy((b + sb->len), s, len);
+    free(sb->b);
+
+    sb->b = b;
+    sb->len = new_len;
+}
+
+void draw_rows(String *sb, int amount)
+{
+    int y;
+    for (y = 0; y < amount; y++) {
+
+        if (y == amount / 3) {
+
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                                      "Text editor %s", VERSION);
+            if (welcomelen > E.screencols)
+                welcomelen = E.screencols;
+
+            int padding = (E.screencols - welcomelen) / 2;
+
+            if (padding > 0) {
+                s_append(sb, "~", 1);
+            }
+
+            while (padding--) {
+                s_append(sb, " ", 1);
+            }
+
+            s_append(sb, welcome, welcomelen);
+
+        }
+        else {
+            s_append(sb, "~", 1);
+        }
+
+        s_append(sb, "\x1b[K", 3);
+        if (y < amount - 1)
+            s_append(sb, "\r\n", 2);
+    }
+}
+
+void refresh_screen(void)
+{
+    String sb = { NULL, 0 };
+    s_append(&sb, "\x1b[?25l\x1b[H", 9);
+    draw_rows(&sb, E.screenrows);
+
+    char buf[100];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    s_append(&sb, buf, strlen(buf));
+
+    s_append(&sb, "\x1b[?25h", 6);
+    write(STDIN_FILENO, sb.b, sb.len);
+}
+
+char read_key(void)
+{
+    char c = '\0';
+    int nread;
+    if ((nread = read(STDIN_FILENO, &c, 1)) < 0) {
+        if (nread < 0 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+void move_cursor(char key)
+{
+    switch (key) {
+    case 'h':
+        if (E.cx > 0)
+            E.cx--;
+        break;
+    case 'j':
+        if (E.cy < E.screenrows)
+            E.cy++;
+        break;
+    case 'k':
+        if (E.cy > 0)
+            E.cy--;
+        break;
+    case 'l':
+        if (E.cx < E.screencols)
+            E.cx++;
+        break;
+    }
+}
+
+void process_keypress(void)
+{
+    char c;
+    c = read_key();
+    switch (c) {
+    case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J\x1b[H", 7);
+        exit(0);
+        break;
+
+    case 'h':
+    case 'j':
+    case 'k':
+    case 'l':
+        move_cursor(c);
+        break;
+    }
+}
+
+void init(void)
+{
+    get_window_size(&E.screenrows, &E.screencols);
+    E.cy = 0;
+    E.cx = 0;
 }
